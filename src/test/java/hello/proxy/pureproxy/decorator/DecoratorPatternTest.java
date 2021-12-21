5e3dcc17fe0b9c92e259a91acc08f5209c437edc
salgu1998 <salgu1998@gmail.com>
2021-12-21 22:16:46 +0900

    add : decorator pattern Test Code

---
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,28 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.Component;
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DecoratorPatternTest {
+
+    @Test
+    public void noDecoTest() {
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient decoratorPatternClient = new DecoratorPatternClient(realComponent);
+        decoratorPatternClient.execute();
+    }
+
+    @Test
+    public void decoTest() {
+        Component realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+}
